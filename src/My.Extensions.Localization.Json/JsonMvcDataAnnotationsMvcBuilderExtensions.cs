99c39d879753f698baabeebc07e737ca072b5c2f
Muhammad Altabba <maltabba@elm.sa>
2019-07-28 13:59:09 +0300

    Support `AddDataAnnotationsJsonLocalization` Added an extention method to `IMvcBuilder` in order to support Localization for Data Annotation

---
+++ src/My.Extensions.Localization.Json/JsonMvcDataAnnotationsMvcBuilderExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+using Microsoft.AspNetCore.Mvc.DataAnnotations;
+using Microsoft.Extensions.Localization;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class JsonLocalizationMvcDataAnnotationsMvcBuilderExtensions
+    {
+        public static IMvcBuilder AddDataAnnotationsJsonLocalization(this IMvcBuilder builder)
+        {
+            if (builder == null)
+            {
+                throw new ArgumentNullException(nameof(builder));
+            }
+            builder.AddDataAnnotationsLocalization(o =>
+            {
+                var provider = builder.Services.BuildServiceProvider();
+                var factory = provider.GetService<IStringLocalizerFactory>();
+                var localizer = provider.GetService<IStringLocalizer>();
+
+                o.DataAnnotationLocalizerProvider = (t, f) => localizer;
+            });
+            return builder;
+        }
+    }
+}
