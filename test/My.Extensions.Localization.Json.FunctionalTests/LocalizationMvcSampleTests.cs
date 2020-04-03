c49ecde6ca0f707d35cd14d8c829917249ff51e9
Hisham Bin Ateya <hishamco_2007@yahoo.com>
2020-04-03 16:36:30 +0300

    Add cookie to HttpClient header

--- test/My.Extensions.Localization.Json.FunctionalTests/LocalizationMvcSampleTests.cs
+++ test/My.Extensions.Localization.Json.FunctionalTests/LocalizationMvcSampleTests.cs
@@ -1,5 +1,7 @@
+using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Localization;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Xunit;
 
@@ -21,12 +23,16 @@ namespace LocalizationSample.Mvc.FunctionalTest
         {
             // Arrange
             var url = "/Home/Privacy";
+            var request = new HttpRequestMessage(HttpMethod.Get, url);
+            var cookieValue = $"c={culture}|uic={culture}";
+            request.Headers.Add("Cookie", $"{CookieRequestCultureProvider.DefaultCookieName}={cookieValue}");
 
             // Act
-            var response = await _client.GetAsync($"{url}/?culture={culture}&ui-culture={culture}");
+            var response = await _client.SendAsync(request);
             var content = await response.Content.ReadAsStringAsync();
 
             // Assert
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
             Assert.Contains(expected, content);
         }
     }
