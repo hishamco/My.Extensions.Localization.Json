8335399deca8e0b7cc95018c029e9c8893d9fe82
Hisham Bin Ateya <hishamco_2007@yahoo.com>
2020-04-03 08:58:48 +0300

    Add Localization Mvc sample

---
+++ samples/LocalizationSample.Mvc/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Localization;
+
+namespace LocalizationSample.Mvc.Controllers
+{
+    [Route("")]
+    public class HomeController : Controller
+    {
+        private readonly IStringLocalizer<HomeController> _localizer;
+
+        public HomeController(IStringLocalizer<HomeController> localizer)
+        {
+            _localizer = localizer ?? throw new ArgumentNullException(nameof(localizer));
+        }
+
+        public string Index()
+        {
+            var result = _localizer["Hello"].Value;
+            
+            return result;
+        }
+    }
+}
