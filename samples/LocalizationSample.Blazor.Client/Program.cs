053e4037cb8049bde4b7f750a417d16eea92c187
hishamco <hishamco_2007@yahoo.com>
2020-10-11 22:23:15 +0300

    Modify blazor client

--- samples/LocalizationSample.Blazor.Client/Program.cs
+++ samples/LocalizationSample.Blazor.Client/Program.cs
@@ -1,12 +1,10 @@
 using System;
-using System.Globalization;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.JSInterop;
 
-namespace LocalizationSample.Blazor
+namespace LocalizationSample.Blazor.Client
 {
     public class Program
     {
@@ -18,15 +16,7 @@ namespace LocalizationSample.Blazor
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
             builder.Services.AddJsonLocalization(options => options.ResourcesPath = "Resources");
 
-            var host = builder.Build();
-            var jsInterop = host.Services.GetRequiredService<IJSRuntime>();
-            var browserLocale = await jsInterop.InvokeAsync<string>("getBrowserLocale");
-            var culture = new CultureInfo(browserLocale);
-
-            CultureInfo.CurrentCulture = culture;
-            CultureInfo.CurrentUICulture = culture;
-
-            await host.RunAsync();
+            await builder.Build().RunAsync();
         }
     }
 }
