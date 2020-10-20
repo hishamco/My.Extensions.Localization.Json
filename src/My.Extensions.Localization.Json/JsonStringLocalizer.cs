f383e009e31a0f217eaebaff5bb76f358f67c5ae
hishamco <hishamco_2007@yahoo.com>
2020-10-21 01:52:59 +0300

    Refactoring

--- src/My.Extensions.Localization.Json/JsonStringLocalizer.cs
+++ src/My.Extensions.Localization.Json/JsonStringLocalizer.cs
@@ -2,10 +2,9 @@
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Globalization;
-using System.IO;
-using System.Linq;
-using Microsoft.Extensions.Configuration;
+using System.Resources;
 using Microsoft.Extensions.Localization;
+using Microsoft.Extensions.Localization.Internal;
 using Microsoft.Extensions.Logging;
 using My.Extensions.Localization.Json.Internal;
 
@@ -13,21 +12,45 @@ namespace My.Extensions.Localization.Json
 {
     public class JsonStringLocalizer : IStringLocalizer
     {
-        private readonly ConcurrentDictionary<string, IEnumerable<KeyValuePair<string, string>>> _resourcesCache = new ConcurrentDictionary<string, IEnumerable<KeyValuePair<string, string>>>();
-        private readonly string _resourcesPath;
-        private readonly string _resourceName;
+        private readonly ConcurrentDictionary<string, object> _missingManifestCache = new ConcurrentDictionary<string, object>();
+        private readonly IResourceNamesCache _resourceNamesCache;
+        private readonly JsonResourceManager _jsonResourceManager;
+        private readonly IResourceStringProvider _resourceStringProvider;
         private readonly ILogger _logger;
 
         private string _searchedLocation;
 
         public JsonStringLocalizer(
-            string resourcesPath,
-            string resourceName, // TODO: Use optional parameter in upcoming major release
+            JsonResourceManager jsonResourceManager,
+            IResourceNamesCache resourceNamesCache,
+            ILogger logger)
+            : this(jsonResourceManager,
+                new JsonStringProvider(resourceNamesCache, jsonResourceManager),
+                resourceNamesCache,
+                logger)
+        {
+
+        }
+
+        public JsonStringLocalizer(
+            JsonResourceManager jsonResourceManager,
+            IResourceStringProvider resourceStringProvider,
+            IResourceNamesCache resourceNamesCache,
             ILogger logger)
         {
-            _resourcesPath = resourcesPath ?? throw new ArgumentNullException(nameof(resourcesPath));
-            _resourceName = resourceName;
+            _jsonResourceManager = jsonResourceManager ?? throw new ArgumentNullException(nameof(jsonResourceManager));
+            _resourceStringProvider = resourceStringProvider ?? throw new ArgumentNullException(nameof(resourceStringProvider));
+            _resourceNamesCache = resourceNamesCache ?? throw new ArgumentNullException(nameof(resourceNamesCache));
             _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+        [Obsolete("This constructor is deprecated and will be removed in the major release.", true)]
+        public JsonStringLocalizer(
+            string resourcesPath,
+            string resourceName,
+            ILogger logger)
+        {
+
         }
 
         public LocalizedString this[string name]
@@ -39,7 +62,7 @@ namespace My.Extensions.Localization.Json
                     throw new ArgumentNullException(nameof(name));
                 }
 
-                var value = GetStringSafely(name);
+                var value = GetStringSafely(name, null);
 
                 return new LocalizedString(name, value ?? name, resourceNotFound: value == null, searchedLocation: _searchedLocation);
             }
@@ -54,7 +77,7 @@ namespace My.Extensions.Localization.Json
                     throw new ArgumentNullException(nameof(name));
                 }
 
-                var format = GetStringSafely(name);
+                var format = GetStringSafely(name, null);
                 var value = string.Format(format ?? name, arguments);
 
                 return new LocalizedString(name, value, resourceNotFound: format == null, searchedLocation: _searchedLocation);
@@ -74,58 +97,58 @@ namespace My.Extensions.Localization.Json
             }
 
             var resourceNames = includeParentCultures
-                ? GetAllStringsFromCultureHierarchy(culture)
-                : GetAllResourceStrings(culture);
+                ? GetResourceNamesFromCultureHierarchy(culture)
+                : _resourceStringProvider.GetAllResourceStrings(culture, true);
 
             foreach (var name in resourceNames)
             {
-                var value = GetStringSafely(name);
+                var value = GetStringSafely(name, culture);
                 yield return new LocalizedString(name, value ?? name, resourceNotFound: value == null, searchedLocation: _searchedLocation);
             }
         }
 
-        // TODO: Remove virtual on the upcoming major release
-        protected virtual string GetStringSafely(string name)
+        [Obsolete("This method is deprecated and will be removed in the major release.")]
+        protected virtual string GetStringSafely(string name) => GetStringSafely(name, CultureInfo.CurrentUICulture);
+
+        protected string GetStringSafely(string name, CultureInfo culture)
         {
             if (name == null)
             {
                 throw new ArgumentNullException(nameof(name));
             }
 
-            var culture = CultureInfo.CurrentUICulture;
-            string value = null;
+            var keyCulture = culture ?? CultureInfo.CurrentUICulture;
+            var cacheKey = $"name={name}&culture={keyCulture.Name}";
 
-            while (culture != culture.Parent)
+            _logger.SearchedLocation(name, _jsonResourceManager.ResourcesFilePath, keyCulture);
+
+            if (_missingManifestCache.ContainsKey(cacheKey))
             {
-                BuildResourcesCache(culture.Name);
-
-                if (_resourcesCache.TryGetValue(culture.Name, out IEnumerable<KeyValuePair<string, string>> resources))
-                {
-                    var resource = resources?.SingleOrDefault(s => s.Key == name);
-
-                    value = resource?.Value ?? null;
-                    _logger.SearchedLocation(name, _searchedLocation, culture);
-
-                    if (value != null)
-                    {
-                        break;
-                    }
-
-                    culture = culture.Parent;
-                }
+                return null;
             }
 
-            return value;
+            try
+            {
+                return culture == null
+                        ? _jsonResourceManager.GetString(name)
+                        : _jsonResourceManager.GetString(name, culture);
+            }
+            catch (MissingManifestResourceException)
+            {
+                _missingManifestCache.TryAdd(cacheKey, null);
+                
+                return null;
+            }
         }
 
-        private IEnumerable<string> GetAllStringsFromCultureHierarchy(CultureInfo startingCulture)
+        private IEnumerable<string> GetResourceNamesFromCultureHierarchy(CultureInfo startingCulture)
         {
             var currentCulture = startingCulture;
             var resourceNames = new HashSet<string>();
 
             while (currentCulture != currentCulture.Parent)
             {
-                var cultureResourceNames = GetAllResourceStrings(currentCulture);
+                var cultureResourceNames = _resourceStringProvider.GetAllResourceStrings(currentCulture, false);
 
                 if (cultureResourceNames != null)
                 {
@@ -139,60 +162,6 @@ namespace My.Extensions.Localization.Json
             }
 
             return resourceNames;
-        }
-
-        private IEnumerable<string> GetAllResourceStrings(CultureInfo culture)
-        {
-            BuildResourcesCache(culture.Name);
-
-            if (_resourcesCache.TryGetValue(culture.Name, out IEnumerable<KeyValuePair<string, string>> resources))
-            {
-                foreach (var resource in resources)
-                {
-                    yield return resource.Key;
-                }
-            }
-            else
-            {
-                yield return null;
-            }
-        }
-
-        private void BuildResourcesCache(string culture)
-        {
-            _resourcesCache.GetOrAdd(culture, _ =>
-            {
-                var resourceFile = string.IsNullOrEmpty(_resourceName)
-                    ? $"{culture}.json"
-                    : $"{_resourceName}.{culture}.json";
-
-                _searchedLocation = Path.Combine( _resourcesPath, resourceFile);
-
-                if (!File.Exists(_searchedLocation))
-                {
-                    if (resourceFile.Count(r => r == '.') > 1)
-                    {
-                        var resourceFileWithoutExtension = Path.GetFileNameWithoutExtension(resourceFile);
-                        var resourceFileWithoutCulture = resourceFileWithoutExtension.Substring(0, resourceFileWithoutExtension.LastIndexOf('.'));
-                        resourceFile = $"{resourceFileWithoutCulture.Replace('.', Path.DirectorySeparatorChar)}.{culture}.json";
-                        _searchedLocation = Path.Combine(_resourcesPath, resourceFile);
-                    }
-                }
-                
-                IEnumerable<KeyValuePair<string, string>> value = null;
-
-                if (File.Exists(_searchedLocation))
-                {
-                    var builder = new ConfigurationBuilder()
-                    .SetBasePath(_resourcesPath)
-                    .AddJsonFile(resourceFile, optional: false, reloadOnChange: true);
-
-                    var config = builder.Build();
-                    value = config.AsEnumerable();
-                }
-
-                return value;
-            });
         }
     }
 }
