e05773db6b68342603bc6e9d7eeb76f987b7bdcf
Hisham Bin Ateya <hishamco_2007@yahoo.com>
2023-02-28 21:30:42 +0300

    Add benchmarks

---
+++ test/My.Extensions.Localization.Json.Benchmarks/JsonResourceManagerBenchmark.cs
@@ -0,0 +1,30 @@
+﻿using BenchmarkDotNet.Attributes;
+using My.Extensions.Localization.Json.Internal;
+using System.Globalization;
+
+namespace My.Extensions.Localization.Json.Benchmarks;
+
+[MemoryDiagnoser]
+public class JsonResourceManagerBenchmark
+{
+    private static readonly JsonResourceManager _jsonResourceManager;
+    private static readonly CultureInfo _frenchCulture;
+
+    static JsonResourceManagerBenchmark()
+    {
+        _jsonResourceManager = new JsonResourceManager("Resources\\fr-FR.json");
+        _frenchCulture = CultureInfo.GetCultureInfo("fr-FR");
+    }
+
+    [Benchmark]
+    public void EvaluateGetResourceSetWithoutCultureFallback()
+    {
+        _jsonResourceManager.GetResourceSet(_frenchCulture, tryParents: false);
+    }
+
+    [Benchmark]
+    public void EvaluateGetResourceSetWithCultureFallback()
+    {
+        _jsonResourceManager.GetResourceSet(_frenchCulture, tryParents: true);
+    }
+}
---
+++ test/My.Extensions.Localization.Json.Benchmarks/Program.cs
@@ -0,0 +1,8 @@
+﻿using BenchmarkDotNet.Running;
+
+namespace My.Extensions.Localization.Json.Benchmarks;
+
+public static class Program
+{
+    public static void Main(string[] args) => BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
+}
