e841fa008f2d72b072dfd5205ceb001cfc509997
Hisham Bin Ateya <hishamco_2007@yahoo.com>
2024-02-23 18:46:25 +0300

    Add JsonResourceManager benchmark between versions

---
+++ test/My.Extensions.Localization.Json.Benchmarks/JsonResourceManagerTests.cs
@@ -0,0 +1,42 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Toolchains.CsProj;
+using My.Extensions.Localization.Json.Internal;
+using System.Globalization;
+
+namespace My.Extensions.Localization.Json.Benchmarks
+{
+    [Config(typeof(BenchmarkConfig))]
+    public class JsonResourceManagerTests
+    {
+        private static readonly JsonResourceManager _jsonResourceManager;
+        private static readonly CultureInfo _frenchCulture;
+
+        static JsonResourceManagerTests()
+        {
+            _jsonResourceManager = new JsonResourceManager("Resources\\fr-FR.json");
+            _frenchCulture = CultureInfo.GetCultureInfo("fr-FR");
+        }
+
+        [Benchmark]
+        public void EvaluateGetResourceSetWithoutCultureFallback()
+            => _jsonResourceManager.GetResourceSet(_frenchCulture, tryParents: false);
+
+        [Benchmark]
+        public void EvaluateGetResourceSetWithCultureFallback()
+            => _jsonResourceManager.GetResourceSet(_frenchCulture, tryParents: true);
+
+        private class BenchmarkConfig : ManualConfig
+        {
+            public BenchmarkConfig()
+            {
+                var baseJob = Job.MediumRun.WithToolchain(CsProjCoreToolchain.NetCoreApp80);
+
+                AddJob(baseJob.WithNuGet("My.Extensions.Localization.Json", "3.0.0"));
+                AddJob(baseJob.WithNuGet("My.Extensions.Localization.Json", "3.1.0"));
+                AddJob(baseJob.WithNuGet("My.Extensions.Localization.Json", "3.2.0"));
+            }
+        }
+    }
+}
