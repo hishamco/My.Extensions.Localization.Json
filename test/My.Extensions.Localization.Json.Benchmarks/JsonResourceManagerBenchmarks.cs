737373e42e57e91abfe7c52d4a22d268e04600d1
Hisham Bin Ateya <hishamco_2007@yahoo.com>
2024-03-25 11:23:03 +0300

    Add version 3.3.0 to JsonResourceManagerBenchmarks

--- test/My.Extensions.Localization.Json.Benchmarks/JsonResourceManagerBenchmarks.cs
+++ test/My.Extensions.Localization.Json.Benchmarks/JsonResourceManagerBenchmarks.cs
@@ -8,12 +8,12 @@ using System.Globalization;
 namespace My.Extensions.Localization.Json.Benchmarks
 {
     [Config(typeof(BenchmarkConfig))]
-    public class JsonResourceManagerTests
+    public class JsonResourceManagerBenchmarks
     {
         private static readonly JsonResourceManager _jsonResourceManager;
         private static readonly CultureInfo _frenchCulture;
 
-        static JsonResourceManagerTests()
+        static JsonResourceManagerBenchmarks()
         {
             _jsonResourceManager = new JsonResourceManager("Resources\\fr-FR.json");
             _frenchCulture = CultureInfo.GetCultureInfo("fr-FR");
@@ -36,6 +36,7 @@ namespace My.Extensions.Localization.Json.Benchmarks
                 AddJob(baseJob.WithNuGet("My.Extensions.Localization.Json", "3.0.0"));
                 AddJob(baseJob.WithNuGet("My.Extensions.Localization.Json", "3.1.0"));
                 AddJob(baseJob.WithNuGet("My.Extensions.Localization.Json", "3.2.0"));
+                AddJob(baseJob.WithNuGet("My.Extensions.Localization.Json", "3.3.0"));
             }
         }
     }
